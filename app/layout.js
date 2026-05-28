c3000e7bb163fd4941b222d719a4f18fa62947cc
dkdesing <konskiyden@gmail.com>
2026-05-28 21:42:57 +0300

    Add Open Graph image

--- app/layout.js
+++ app/layout.js
@@ -15,5 +15,27 @@ export const metadata = {
     "дизайн квартири",
     "DK Design"
   ],
+openGraph: {
+  title: "DK Design | Дизайн інтер’єру",
+
+  description:
+    "Сучасний дизайн інтер’єру, 3D візуалізації та креслення.",
+
+  url: "https://designdkk.com",
+
+  siteName: "DK Design",
+
+  images: [
+    {
+      url: "/og-image.jpg",
+      width: 1200,
+      height: 630,
+      alt: "DK Design",
+    },
+  ],
+
+  locale: "uk_UA",
+  type: "website",
+},
 }
 export default function RootLayout({children}){return <html lang="uk"><body>{children}<Analytics /></body></html>}
